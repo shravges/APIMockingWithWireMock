5aba79b2dad32385a60153ac99490626a42ab948
Shilpa S <shilpa.shravge@numito.com>
2026-02-06 21:21:06 +0000

    added code for field verification

--- src/test/java/testMockAPIs/testGetMockAPI.java
+++ src/test/java/testMockAPIs/testGetMockAPI.java
@@ -6,6 +6,7 @@ import APIMocking.APIMocks;
 import base.BaseTest;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import static org.hamcrest.Matchers.*;
 
 public class testGetMockAPI extends BaseTest {
 
@@ -17,11 +18,24 @@ public class testGetMockAPI extends BaseTest {
 		//call the method to create a get call
 		APIMocks.getDummyUser();
 		
-		Response response = RestAssured.given()
-				    .when()
-					.get("/api/users/555");
-					
-		response.prettyPrint();
+//		Response response = RestAssured.given()
+//				    .when()
+//					.get("/api/users/555");
+//					
+//		response.prettyPrint();
+		
+		
+		RestAssured.given()
+	    			.when()
+	    			.get("/api/users/555")
+	    			.then()
+	    			.log().all()
+	    			.statusCode(200)
+	    			.body("name", equalTo("Shilpa"))
+	    			.and()
+	    			.body("role", equalTo("Test Manager"))
+	    			.and()
+	    			.body("id", equalTo("555"));
 									
 	}
 	
